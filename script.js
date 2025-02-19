e36c694d98588de2a5e42428d4fc1cbca9df2e51
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2025-02-19 13:18:45 +0100

    added JS for popup form, and made form responsive for all screens

---
+++ script.js
@@ -0,0 +1,26 @@
+let popupClosed = false;
+
+const closeButton = document.getElementById('close-popup');
+  const overlay = document.getElementById('overlay');
+  const popupCard = document.getElementById('popup-card');
+
+window.addEventListener('scroll', function() {
+  const hrElement = document.querySelector('hr');
+
+  if (!popupClosed && window.scrollY + window.innerHeight > hrElement.offsetTop) {
+    overlay.classList.add('show-overlay');
+    popupCard.classList.add('show-popup');
+  }
+});
+
+closeButton.addEventListener('click', function() {
+  overlay.classList.remove('show-overlay');
+  popupCard.classList.remove('show-popup');
+  popupClosed = true;
+});
+
+overlay.addEventListener('click', function() {
+  overlay.classList.remove('show-overlay');
+  popupCard.classList.remove('show-popup');
+  popupClosed = true;
+});
